df33b2f64ff75a74e0d892fe3bb06432674a6c8e
Alexei <lexas427@gmail.com>
2022-12-11 23:32:15 +0300

    feat: implement day3 solution

---
+++ day3.js
@@ -0,0 +1,342 @@
+const input = 'GwrhJPDJCZFRcwfZWV\n' +
+    'LjnQlqNpjjmpmQlLlqNfZRvQcTWcTSTTZcSQcZ\n' +
+    'nNqjdspspngnmjmslqmjjjCDGrHPHMGddGCMCGPPPJWC\n' +
+    'GwmVZmPWWFFmBbVbZVwmbPsTCnlgQgnQfhlffffZnlQh\n' +
+    'DqVDSqqSMzLLDDNSHHLdqSdSllCQjsTlClhlflnTlhjgfgfM\n' +
+    'VHJztNLHGtcbvvPG\n' +
+    'bjrPrNCtNrjdcCPpptfpTVspDtfTtB\n' +
+    'JGQJMJQMmmmZMnnLpLBTpHCD\n' +
+    'WJJqWRgWlCJZhZRCQZwdPScdrPNbvzPzwvqz\n' +
+    'QNSQNBWQNLjZBNNhLhSNRsTcsrTTVzcwZZZsfrrbwb\n' +
+    'tCFtHpppppMldpvpqnMFmMVGrbPcrwbzswrzcccfvTfw\n' +
+    'pdmCpgqCdmHHdJVWgSRNJDRVVj\n' +
+    'sNrFnvNSzrjQtQjQTj\n' +
+    'lcPmcJDLdPDbJPVLljdGGBBThBQTGwTtBw\n' +
+    'PDLqmJmpJQfFqfqsCM\n' +
+    'BnhctqdnqnRcBnslCJJCMrJrsG\n' +
+    'wNDMZpbQwMpCvCGVjlss\n' +
+    'WfzNwZFbwZzZmFZbLzNwzzzzcdqgRMTTPdHPTTPMRdcWgRPt\n' +
+    'grsrVSFSSdFSDFVFjZZWwpWpZWZplgZZ\n' +
+    'mcBPPPBLBfNdLlvvWljWJC\n' +
+    'dMcmcRdbRzdVhFthSsTShM\n' +
+    'bzvJZMTzTZSHLCCdDzmDcc\n' +
+    'hqBqWPFssvshWvvssNqtsHftmfpHfdcdDGHmcpfctL\n' +
+    'WvBQgNNNhghTJbJQlJTZlT\n' +
+    'chcdwNwdbCbQctCjnnQGHsQspMHMjG\n' +
+    'nSSSJqJZzJgWWRfZDJSnqvTTsVvvHVPpHVfpjHMTjP\n' +
+    'BZRDRmmrDWSrZWWzWSRNhdnCFwChclFtwbNdtr\n' +
+    'lNgmssCtqLwqCCtfsCLHPFhhhmMhVzBDbVzMDMVz\n' +
+    'ZnRlQTlJzFQFQFVV\n' +
+    'npZJvRRGZSnWvSvrSLglsClfpfcLgNgpHf\n' +
+    'tVtqcVqFVtZhcfFtqlgSpmpDSDNPzSzZmNpw\n' +
+    'LRGTHqbrHdnGHrTCSSwNDzMDwPMzNwbp\n' +
+    'TqWGJrGHCHnTWnhsWcFthFjtfQch\n' +
+    'qNnTbwtctvffFcqfrHjMrFjVHRjSjZDZ\n' +
+    'dLLzWWPmCmCzGdsLgBLGGBDRMVMHRlrrrZDDZsNMrNNS\n' +
+    'PJQWggCzWNWJzGWfchvfTbJvfnnwtf\n' +
+    'ghzdgzzdQsdqzzhMNqQzvhgQnRRBWTjWWGTRGWwGTZhwGnBT\n' +
+    'fsrfJHbFfDFLVLVFHrWCWrBRZZTGCCjwWZ\n' +
+    'HLLllcDPbLPQdPspMNgvMt\n' +
+    'fNDJqdPNbtHpCbwpCCCp\n' +
+    'RTMRLrzGrMRMRPWnnvSmgCHFCCFmmT\n' +
+    'WQsWQjzGWMsGQzWclQtVBJfBftNdtqVPfP\n' +
+    'gbTCVVmDVFdsgmgrrcfwlwfTfPlcRR\n' +
+    'qhQZqQvnQhLQhJnvfPcSwSwlfjGcqjqj\n' +
+    'tLNZLZZJJZthpzhMZDCdFmFsmWWmtDDgsw\n' +
+    'bqCvLvLppzPzPPvPbFztFtttBNGdGsRggSgGSHDdggHSzNgH\n' +
+    'rMQpWfMfrcTjWJhwWHHsSBsRBdSTTNBgSR\n' +
+    'VwfmWjwMWwccrWcWpQQFnFtlCqmltFnFLbbZmn\n' +
+    'cWqsMWJMzqJJMHsJcqsJqTqjSbLBdfdSbtzLbbLfbSfShfhd\n' +
+    'gplGvQmRrCrgZSZtSGZZjhbj\n' +
+    'CQmmmmNQRPvjgRClCvmmcVHPqMFMFsWJVqFFcnTJ\n' +
+    'QHHqvGwjjWNqvGTQGvTFcGwJRJbszcPtDbJVbtPzVbDptp\n' +
+    'MLdrgmSgZZdhdfbLVRpszlRDstRL\n' +
+    'gdSgMCSfdMnrghCWGRQvHwvNHjnjvv\n' +
+    'RDBZwvZBrMlsvnlb\n' +
+    'WdFQqdjWWcHHPrwSPnnSWnSS\n' +
+    'mLdqgqHmcjHHjqLHjLppmhfBfgtDtBJZJfVtBZwGZB\n' +
+    'CCWRJQnZlHtHtNZRFDcBhrcvhDrJVVDv\n' +
+    'dPPSqLzfsqGLSTzfLzLGdLMVVgvBcmgMVwmmDFrVgmBBBr\n' +
+    'SFjdTGzqpjdRbNRNnjtnQR\n' +
+    'hjNcwBDDwDFcjdFfjtFhtcRsGGgTsGRRRTsGGqZGRq\n' +
+    'gbmrLnbzLmvQJnQVVpqZTqzWSCRpqRTsSR\n' +
+    'MQMvVMbPQQHrQMnMPldtwNNfgHtlwBhdwj\n' +
+    'zwzwpzMfzrBMWfCCZrwzrMJDGGGnNmGNZvgNZsDDsGsG\n' +
+    'FbFqSbcSbSHqTjmgGFnJglllsDJm\n' +
+    'TbhVdVjqdtqTjVHqjPdthPBBWpCnRfwRPRCfBCCnWR\n' +
+    'hlpmbfJJpCSChmJMmrSjTjcSdjTtQQTtTtjF\n' +
+    'gqrgsqLzgnBgZGzHBnnsQNNQtjjcNNjjtNFQNcNH\n' +
+    'LVRzgGGzzzPCVrJMbPJb\n' +
+    'VHrmqFnVdvlzzNrr\n' +
+    'PMtwBJPBcPwfbwBJndplLvLdLlgMMzLL\n' +
+    'bBZnTwbtnScfQJPJwPTjqGZFsVFjDHHGhhHhVj\n' +
+    'cftqScHJrfVfrrRZ\n' +
+    'DTTsDvvlBbTGrWBwwsWDBbWdVpZjjZjpVPPGhRRVjVZNRPNN\n' +
+    'lsWdWDbrTLBsbdrmdwbMJtmHMQJccFHFnJFqFt\n' +
+    'SWNPTPVSWChCSmQQhpppJdFJLpDpgLJmLd\n' +
+    'NGGtNtGfHtDpdJdqLB\n' +
+    'NcsNGNjHZsZGnzZfnGhQnhPClrVlQPhTVVhl\n' +
+    'QDdgMBsNhhMgcWbZdzmWLzFzWH\n' +
+    'fRqRJJqGCvrJGjCRRrSJlfPtHzzPmfFbtPtLZZLnmt\n' +
+    'VjvwwjlwVGGqJSSqJFccshpgNhQNQTsVgBgT\n' +
+    'wvDLDwCbFgSTfTSJJgfB\n' +
+    'qsRhmhqchmVhPdfTHJSzpCtJpfPf\n' +
+    'hmdhrWrddmhlqCRcwQjDLMQnMFDZnlLl\n' +
+    'trMWtlwwMplMZMCZWltDpzBLBnflVLBbHzbBSGlVlL\n' +
+    'ghhqJTfmjQjfqqznznnHnBRzBLmn\n' +
+    'sQhPQsjjQcQcTsPqZWwwZcFfWrWcrZww\n' +
+    'MRVpVCZZTHWVMCHvgNvVvbQSqgQSlg\n' +
+    'NFmnrNDDfnjFnndfssmcStvjvQQlvzvllqvwQllj\n' +
+    'GGPNmBrFNdcfcGrsGcdmDFhJHMMhHLZJMhpLHCMMMMPJ\n' +
+    'DSvDGdGFlGGnDZFdVSZvfPqwnfhpnrqpPNpLPrrh\n' +
+    'sWcTjtHCsTmsCNfgMPjpfPhqhP\n' +
+    'BtHzBzChzBBvFSDJvVzFJJ\n' +
+    'sfsNrsFFBTfjwwtNNWHPVCVWtSCDDCDmmS\n' +
+    'zMdhMMZnSccMmmWVWmCPlC\n' +
+    'cLSScJZQbcvLhZvnzBwfTjrpNwNrBFffpb\n' +
+    'TBrCBgrTngVQBVbhrCtgJJrGssGsMGRGcjMcNjfN\n' +
+    'LZdSLvHMFdzFRWsLjcGRWWNJ\n' +
+    'pHpzlqPqFPvdBthgMbVPDhgh\n' +
+    'SZlnZZvBvvMrcBnllBMZSvhGMtQwFMGztthfwQtMwwPf\n' +
+    'HLqsDgNsDLDDDjggHDHszthzFbQGTghPGQPbTfFT\n' +
+    'dmLqDqCmFNjJsjHdssFNHDVWZccnRllnVZvRSBZrZlCc\n' +
+    'SccnnSGGftShfHSHHhnvbMjvVlCjzbVzzbMMTbCB\n' +
+    'gRpppNNQLWqZgPZwNWwwBMBbDlZCTzVTjHMMbBjV\n' +
+    'dqNQPQRqrqpPcGtchhdfhHSF\n' +
+    'mfDzgnNMMszBtJCpHlrjnFppCdHj\n' +
+    'LLRThGGZcbClBQpdWFGl\n' +
+    'bSqVTbBbMVMsNmNM\n' +
+    'BTTbbLVpfchmjbsj\n' +
+    'JSQJHDMHqdNZTZlhFFhCFFrNhNcsrr\n' +
+    'tMwJQlwMMlQwDDJtWGLGPpWLLGnTPn\n' +
+    'LcVQQCPPLqTzqQTcllTzhnHHfFJRcGHcFfwRGHwJjJ\n' +
+    'stdWDDBtVgbpWgZbsNgDNdWFGMnnwHfjHFpfwwMGMMGRjJ\n' +
+    'ZWSDtgNdWNBdgsdsNDDsdbDlTzCVSTCqQmSqTQSvhqLVQq\n' +
+    'dZbgdZbNtmqttFJtHHzcczMcFszHnsvH\n' +
+    'wwpQplQQwqVVjqwPjCGCSMCMcHSHvvzHMzvcsrMc\n' +
+    'pfjlQRpPRRLQWtmLNdWdmqqJ\n' +
+    'CPTPPmbjmVjVGCvzbjjPrGsnnMpttdtGdncdMccDRd\n' +
+    'lhlHzQSHwzhJLwgWgpMDMMsDdcDQMDMMns\n' +
+    'BHZghLWwSFBJJBFvzmbfjNZvZmCvmb\n' +
+    'PBGcvvcRwpwNcZcNPpPNcTHGdMtrCWrCCtCLWMtWgbVdMV\n' +
+    'fmsJjnqmmfsjQJnjFzSFSqsqgWrtMttZgMWVMbbVMdbSrLtr\n' +
+    'qQjjZFmfjZhZmwcvPhNpTNBTwN\n' +
+    'HHlVVmmsbbqMsJmVzGSBMSrQQrRrGvvnDn\n' +
+    'PZcphZPPZPhjcpdWgPZhRPfcDSrtDBSGNvtggrQtnvQNGNDn\n' +
+    'dcWwFjpcPhRcCpjwdCPLzHblJbLbzmsmbTwzqH\n' +
+    'hRfzTTfRrTGzhGWTrRrbfcQZQSttWtwddJtvdJJvWSHq\n' +
+    'npjnDjFlpDnFFNMjljCnFMQtHHtqNHNQJwwZZqstNwJJ\n' +
+    'DCjpLjjpVLDMDpVLDLQbbhzBhVrcVgVGQQcz\n' +
+    'LncLBLjCSNrNrNpCLQBBBGwqQwzlzmggvqRqgllmzwtv\n' +
+    'fMZPHhhHfthMdbRgHJzmVqlvwlwg\n' +
+    'hfsPbZFPPDsfGLcBtSFNBSjL\n' +
+    'MlZmszBMJBHrMBMbShwSFpbZSZfwwb\n' +
+    'TCLCcPNGTgTPNGWtCtcWtPcSsRfRjRwjFbfpNFDjwsFspw\n' +
+    'nVtqqsWsdHzJHqmM\n' +
+    'RCrhSmWrmrvmrvhMvRNrRCzCJcQQbPtsMZVGJJtsZssPcQcZ\n' +
+    'jLFBGqLFpqBLgZVbPbsLJQcbsV\n' +
+    'HjDljGFwrRHRRTrS\n' +
+    'GZZhnrwZBwNjRPRCbCbn\n' +
+    'fJtJJpsVfpgNTbVNFTRP\n' +
+    'JJcpLJfLdcWLdplwRdQMBvSqwRhvrG\n' +
+    'wmZDPlRlCDwglgsHtsBvdBHLFLSddr\n' +
+    'VbVMnMftfVjQWFFHdMBdBFMFHr\n' +
+    'zfjtnGqqnjGqfjPcDPlZPlRDzccw\n' +
+    'BRjhfhvRgnTMlFDDJfZzZFFQDZ\n' +
+    'qLdqcNttwwcwwSPSpqLNmrwmrZsGzzDFZGZFzVssrzJGnsQG\n' +
+    'wSNdHScScdmwHSpdNcmmtLMvChRHbvBMTBnCBBvhvlCh\n' +
+    'JgWTPfFPgCPPlCntQSGghHvQnSdQ\n' +
+    'BzvMZvLVQpdQpSZh\n' +
+    'RwVVjRDVcRDNDTlJPqTv\n' +
+    'SGHSrBBRPhPPHQcTccQTRRQjTN\n' +
+    'vvWvspCbzWVWVrWdjj\n' +
+    'wZpDzCDgDbCZJZzJGlrlqPqnqPllmH\n' +
+    'FCncCrDWMLCbjMCcFpLdzZfmZzwwWzdzNRZdWB\n' +
+    'sqsgTqHSqllNldMwlZzJ\n' +
+    'MtHPTgQhvhhqcrDrrDpjLCQc\n' +
+    'pPPvmPWSClqqPvqCmSwqmgGBWDjhGLHfjhDLJGjBBhNj\n' +
+    'zrbdcdMndcRdTrsMcbTRdzRFVHjLjDjNLNHsfDhNGjhJNhDj\n' +
+    'RdFFcnTdZcTrRRdFFbZtwQCPQglvPlwJwQPZSqqP\n' +
+    'wlmbvwmvQvWQsvmbsSsQbswlRCNPfCTcTRVCffPtTSCPNRVP\n' +
+    'FhJJJFgFqJGBtDpJhTTcVcVhdcCdCdTV\n' +
+    'GDFtgLFnqqDGqGZsQvsllrjbLjbrvw\n' +
+    'lnFSnJvmgvLlfnJpgnsjnjgfDQWqCJqZdDtDCtCtCdDrtDDQ\n' +
+    'VTBBMPFcNNtMZDMW\n' +
+    'VTGbzGGhTbTGHwVPvvFnfpvjgHnfjppp\n' +
+    'JJwHqvlvDjljDwJFlZjZDwHNNsMqhNpphNpmNVzpsnsnRV\n' +
+    'mTLgrLLcLSTTTdmPPfrrrnssNhRNWhgngzMWzgzVnM\n' +
+    'SmTfdSBbBJbtjJvljl\n' +
+    'bPNLwTCLLQQqtJsf\n' +
+    'zdnnZVlWWGGRWGWdgdSStQMqJSMRptftbsMf\n' +
+    'FWbvgvZZZZgnTmwrrhrFPCrP\n' +
+    'HcGzzszFGllHWHbZspHbHGsHTwwrTrLLCNjSZwNjNjjCCNLj\n' +
+    'PBJMJQJDDDnDggRhMdRSLmjTmTwwVjVQSvvwvC\n' +
+    'RqfdhgDPDJDqJJnBdfzWWHcstslcbtStfHzl\n' +
+    'zvRRlCqrdNdZcZpjBpVwjsmjsm\n' +
+    'fgbTDqbhGfDnLDnLLqLhFmsHpTPHjHppppBwpwws\n' +
+    'nhnnnDDngDtDbfSbDnGhhgRlNvQdQqNvQvtcQQNJRNJN\n' +
+    'cZbCcbbScCbcmPGjPfSBQQSq\n' +
+    'lnMnnVsMVvmzzGMDzPDf\n' +
+    'LhrTsTTglrnsrrWWVvlwTnNtcpZRCmhtbCZFdttZbRCp\n' +
+    'NWrFPZVWNVrvvrhtnNdddtpldmjm\n' +
+    'DcBQBDsJbCwQnbtdzmjjjljbpjbz\n' +
+    'qCDcGsDJGCcBDBcswJnBJQDfWfqgvZSvgZPfrVSWvPvZZZ\n' +
+    'vcsdHdGtHtMHMFtVsddsWCcbppZwjScLpWhbjRWR\n' +
+    'NTwrnzJrgTPrDwnlphRpjSpWbJJLLZWj\n' +
+    'TlDPfPnzzlzTBzzvQFFBHMtVtqBqqw\n' +
+    'NHnqqfZvZBNHHvgfrSlJrJCSllJRVrCn\n' +
+    'TDTdhLMWjFcddMJPSSPJRmlCPz\n' +
+    'bljWFdLLTDLtdFtLlwZvqfbgwwHfwqHNvw\n' +
+    'BRRjhRQndRNVqBjRVhFLccjpwMmLmjHmgFHH\n' +
+    'fZJfJvzPPWtWWlltZzZPpcgFMsFFwwFdpHdgwtdw\n' +
+    'PCrdrzzfWCPdvSlqTqNSDnnQVVQQGT\n' +
+    'DjbfBMDSfBljBsLSjSZbzrGtPtMCPtVPvvqrzqzG\n' +
+    'mWdJWcppcNTdpppjzjRRVrPRpq\n' +
+    'QncmnHwmdTmwQcmjNTfgfhlBShshhsffnfbB\n' +
+    'WGDsMJsrjHCWtDMGDDVQqSvZqfSJzSnvnvvv\n' +
+    'LgLFLFBFLVVzfBzMqZ\n' +
+    'lgmFcwLhNcwdwwMLwhmcRDjNpCWRsWRspGGssHCp\n' +
+    'PnPzNccnjFfvCvhbSBVcWqdhSVhV\n' +
+    'psGMDQJDDDJgQNDHHJbwqwBsVqqZVWBBhBdd\n' +
+    'DlDJDQGptpgpGDfTRnrTrFPnNTlf\n' +
+    'MSSSMLLmFHcDScSq\n' +
+    'ppZnCsbjPZpnnJcbRDmzHJqRRD\n' +
+    'pmNmnGnQNnClZGMVMdBGrMgVWg\n' +
+    'lsTTGcQzBcljCcQzGcGjGptttpmvSJtmggtwwswwtS\n' +
+    'qZRnrhMbRVdhZRhhdnnVRPbmwSNwNNHtmJBvwpvtwNSvSb\n' +
+    'VnMrqrrdqhZrnrBLLlzzlQjQjLfTcGfFDF\n' +
+    'dJJTlHvhZqZlQTJnSgQDzgsSbScsSBzc\n' +
+    'RRNtGjCCpRPPpRtjfrttRzmbscLsLZLgcsbmLzSGLB\n' +
+    'wfNttfNrtWwPNNFfRtpfrdJMTTTZTMZTTVTlVwTlvM\n' +
+    'PQTGLmdNTgPmGgNNdCPLQlrMqBrDzMCMFqDqFqjVCBCD\n' +
+    'hhRwwvpSFmzDrmFh\n' +
+    'vwwZfSfsmvtSspnZLLLdLGWPTGTQtTWG\n' +
+    'pMcWzWFvWhFpPMWzvvhpdprHTZTQrHrQdZTJdfTgQTnJ\n' +
+    'CGbjBbNjjDmRHJDgrTVVZg\n' +
+    'NNttGlGqNLsbtlhMFMFcMLwMvvZz\n' +
+    'CGSCBNCQBtBCQttBwCGtGtQrqrLrJqZHLHbqHvLDHLrq\n' +
+    'nVVhPMfVdfVPbfqLLqgDDqPvgZsv\n' +
+    'cpVncbfnhFcBltTplpmTBC\n' +
+    'MrdcdStbMnddtRBdqMnFmbqGCwqCVHVsNHwPfGVPqsCsCs\n' +
+    'DBLllzWWQQzlZVVVCsGWHfsH\n' +
+    'JQphjTgBjlLgjjpTpLgvTjQnnnSJJRRFmdbRRSdMRtmdMc\n' +
+    'QbRZMSWMblwLsgpwZzqZ\n' +
+    'BFncBrfcdNrrnVrNjsFzFTJpJLGJsGqLTp\n' +
+    'VjhDDBdrfdhQMllzHmPQMh\n' +
+    'LdVVjFVFbpVGRQGllG\n' +
+    'cNMcJNHzJWJtCWHNJHcHczWpGmmhMQmBBqrlRhBmpGpGBQ\n' +
+    'JZzTTtCZtHCJnNnNwPfbFpnfdDdLdnvP\n' +
+    'TpMlrWTTddjmlmDmgQgRtw\n' +
+    'MNNVMSsVSNSnNVMFLDqwtGgRRtGbgFRwtR\n' +
+    'CCLSCPSCZZHVCfZscBJJhPphpdpprdhjJM\n' +
+    'gSMSHJHsMMpzRgHzsRMPPSzsPhtZtZdqdDqQDhdCdZmQldht\n' +
+    'FCcCnrGcNTfvvtqqfvlflQ\n' +
+    'TrTrWNWwrTJLMzJCzWLL\n' +
+    'TpTzwMrfbrpFpMbFrrrzbPSdZmtSZRTlTZRlmdCVlCtJ\n' +
+    'vqvWgqDJQJsQCVtZgdZdRRGd\n' +
+    'vsvLJLchWBcqnvczwjLfzPjfrjzPrz\n' +
+    'zqzbqCFZgmzzmNmf\n' +
+    'vpRWSbRVbVWddVpwvwdRSwnSNgLHsnfNgMmgMLMmnrns\n' +
+    'DwWVpJRlpdbpRDWdGJGcGlhFtPPCqCCBFqZPQttlqFBq\n' +
+    'wQRlwtBJBDwttJdGvLfBvHLLfTLz\n' +
+    'MMmNZcMrcMFnRHzfjjvvHfvc\n' +
+    'FggpbFnhrNNrrMrMbMbnhQVJVhstJwqWCVCRsQJQ\n' +
+    'DQbCGblQlpQFQlHjCbjwDQQMggNmJmgnnpRBngfZmNgJMf\n' +
+    'zvhWccWVdWBchdssPrrWZZZfmsmmmgsnZZJRsRTf\n' +
+    'zBdtqPccWPHFCqCCqljq\n' +
+    'ttrbRMmgtHgfmHSfBpLfnBBZBppB\n' +
+    'CVTJDCCNPwCPDwcqzmddQZdTQdnLBQThWp\n' +
+    'zwFDjwDJJPzjzVNcVJwCcbRHGmbbMrFHgHvrsgbblG\n' +
+    'gZjjwHqHCzrMZVVR\n' +
+    'hhzcdTzPrVhVCGMb\n' +
+    'fPcmLPNffsccJDdNDjBnpwzmHqgWjHwwvg\n' +
+    'SJQFSvQBlzbSCgdPPddPPPSN\n' +
+    'pcrjcWLwwcHcgPNgTPLMNTCB\n' +
+    'pRsjsWRnrpHRmrBrHrjlbJFvvzQFnzQblQDDbJ\n' +
+    'VjQVMQPVMfVPPbGPHHbGJD\n' +
+    'pcqSttltsbDGddsCJG\n' +
+    'TSchqLtTLFhgQbMMQMrr\n' +
+    'trqzMRwNTtDzLPJQgWmjmjrf\n' +
+    'lbBQdpZbsmhGmZhmmG\n' +
+    'llVbpCplvvHBBHpnRDcDRRqnRRQnFRzT\n' +
+    'SLSSFFmzLShsVSSHnLnrJdbnRdZZbrRw\n' +
+    'qCfWBftpNWNNlqvTpwrRbGGCnwGmgRJGZn\n' +
+    'NcTBNpvWvBWpMftNffpqWlTpmzPDQPSzFVMsFQVhHsjHszss\n' +
+    'VtJtNBRBGDpdpNbC\n' +
+    'QgLncnttvFcwwhLvFjSGsSbmmQCSDdpCmpdG\n' +
+    'vLgjLhhrctMvLFFjLtMTLMgfPZqBZPZzJBBfWZZPRZZTRV\n' +
+    'mJzDJJpJBvfsGMQnBM\n' +
+    'CwPWCLRRWwRqwPqhPsrZrnrlhhQrMTrvZl\n' +
+    'dCdLLSPRLSqWqVSLqLjgJDzDmtbngFVtJtzz\n' +
+    'mtgWtMWrqjzQTTjghwwfczlNJdlcJnlc\n' +
+    'FvRsDPPFGRBFvvslwDnTlcTTdwndlh\n' +
+    'SGBZRBTsFGBRvLpvSCmgQWQjgggMrQjmmSmW\n' +
+    'GcsRrQhrVVjhRcWlnDFGGmvntDWZ\n' +
+    'TPbSgJJgBSCbCTbLHMCMTTZdFHvtZlWZDZFzmzZHZmmF\n' +
+    'gBCMCSpbPMMPjcjqQQpqQprv\n' +
+    'nZJcnZwvwzvTTTVtpDFnHH\n' +
+    'DQPBqGGGdMdTRHRBpNgFNR\n' +
+    'dCGPfhPWQdWWWCWShWPqrChWLLwLswjcvSJbvbLjJLbzJbJD\n' +
+    'QrBQtdtrQBrdtFHPrdQBDvGhLGnPnCWnmpDmLpmD\n' +
+    'NjlRJRlNzJJVbSSRVZwwJcmpWDGCWnbchnLCCmnWCG\n' +
+    'llSJzsZzMMlsSZjSjZwJNQqtHHdBFsqdfTHhqFftQB\n' +
+    'zdTJFHTdDBzrNdMnhNnNdM\n' +
+    'ZlLZZcLtVtcWtGjtzLjLZjCrnVNrnRbrQQbQSRVrRnSNqS\n' +
+    'lZtGtCvjZPCGCctPpsDDBzTHFmPmFszD\n' +
+    'mQSMvdMQtQdZhQrPWCPqPQrN\n' +
+    'RwjwnZGzJFTZgzggzJDDwJnCPPhNNqPrLhrGNcWcWNPqCq\n' +
+    'ZTzDfnwFzTngTwJvfSlMtMMlmsHmHt\n' +
+    'lZlmFRVZWmgQWhRsRpJsCJpJct\n' +
+    'PTbPTGTGwwGrbdfjNNZJvcCsCZtvpTsh\n' +
+    'bGdBBqGrdBPjDMzzVFZgqQzFFL\n' +
+    'szvsmLvppPPtzGLGWpVdTSHTNgjHQRmHTgSH\n' +
+    'FnBMBNZwZNcnDZMcnZlZgwgdQTTHjVJjHHVRQHJj\n' +
+    'DnZrFCMZMNffrLPbLsfW\n' +
+    'rJvmnBgnrCrGRSGNQR\n' +
+    'hthjNfhwctwpjTLtVLjTGSpldSCGSPdlPSRzSqSz\n' +
+    'TVcTfHNFcwtjMhTvgbHZsBbWmmZbnH\n' +
+    'WsQgstQmvQJnssWsWPzhRzhBjZBSBRZSnj\n' +
+    'qwCNqFwDrrlDrFPvRhTSPPzLRz\n' +
+    'bppqwppCddlvfbDNVgmMmtMfVVmfmVWW'
+
+const splitInput = input.split('\n');
+
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r',  's', 't', 'u', 'v', 'w', 'x',
+    'x', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z' ];
+
+
+const checkRucksack = (rucksack) => {
+    rucksack = rucksack.split('');
+    const firstPart = rucksack.splice(0, rucksack.length / 2);
+    const secondPart = rucksack;
+    const compare = firstPart.find((e) => secondPart.includes(e));
+    return alphabet.indexOf(compare) + 1;
+}
+
+const filteredRucksacks = splitInput.map(checkRucksack);
+
+console.log(filteredRucksacks.reduce((acc, cur) => acc + cur, 0));
+
+function sliceIntoChunks(arr) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += 3) {
+        const chunk = arr.slice(i, i + 3);
+        res.push(chunk);
+    }
+    return res;
+}
+
+const elvesGroups = sliceIntoChunks(splitInput);
+
+const findBadge = (rucksackGroup) => {
+    const firstPart = rucksackGroup[0].split('');
+    const secondPart = rucksackGroup[1];
+    const thirdPart = rucksackGroup[2];
+    const compare = firstPart.find((e) => secondPart.includes(e) && thirdPart.includes(e));
+    return alphabet.indexOf(compare) + 1;
+}
+
+console.log(elvesGroups.map(findBadge).reduce((acc, cur) => acc + cur, 0));
